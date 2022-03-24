56f081841d417ad10a2879cd57f36c0bf7baf07b
Alexander Chaschin <ac@happynumbers.com>
2022-03-24 15:13:00 +0300

    test: add PrefixKeysDeep

--- types/helpers/tests/Prefix.test.ts
+++ types/helpers/tests/Prefix.test.ts
@@ -1,7 +1,7 @@
 import PrefixKeys, { PrefixKeysDeep } from '@helpers/PrefixKeys';
 import Expand from '@helpers/Expand';
 
-interface Test {
+interface Test1 {
   a: number;
   b: {
     c: string;
@@ -9,4 +9,17 @@ interface Test {
 }
 
 // $ExpectType { "PRE.a": number; "PRE.b": { c: string; }; }
-type T1 = Expand<PrefixKeys<Test, 'PRE'>>;
+type T1 = Expand<PrefixKeys<Test1, 'PRE'>>;
+
+interface Test2 {
+  a: number;
+  b: {
+    c: string;
+    d: {
+      e: boolean;
+    }
+  }
+}
+
+// $ExpectType { a: number; b: { "b.c": string; "b.d": { "b.d.e": boolean; }; }; }
+type T2 = Expand<PrefixKeysDeep<Test2>>;
